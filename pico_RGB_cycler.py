275d5ed21670ac906ff224fd32b9cb49d8986e01
Eric Wilbanks <ericwilbanks@hotmail.com>
2022-11-04 16:21:41 -0500

    Cleaning things up and commenting

--- pico_RGB_cycler.py
+++ pico_RGB_cycler.py
@@ -13,17 +13,22 @@ red = PWM(Pin(4, Pin.OUT))
 blue  = PWM(Pin(5, Pin.OUT))
 onboard_led = Pin(25, Pin.OUT)
 
+# Debug Flag, to see what's happening in the backgroud
+global debug
+debug = False
+
 # Setting up a variable to check to see if the button has been pressed and released.
 global button_pressed
 button_pressed = True
 
 # Setting a default color pattern
+global active_color
 active_color = 0
 
 global colors
 colors = OrderedDict(
     [
-        ("off",			 [0, 0 ,0])
+        # ("off",			 [0, 0 ,0]),
         ("white",        [128, 128, 128]),
         ("fast_rainbow", ['x', 'x', 'x']),
         ("slow_rainbow", ['y', 'y', 'y']),
@@ -56,22 +61,17 @@ def rgb_wave(index):
         
 
 def set_color(color_list):
-    # resetting the button value
-    # global button_pressed
-    # button_pressed = False
-     
-    # print("green -"+str(color_list[0]))
+    if debug == True: print("\tgreen - "+str(color_list[0]))
     green.duty_u16(color_list[0] * 256)
 
-    # print("red -"+str(color_list[1]))
+    if debug == True: print("\tred - "+str(color_list[1]))
     red.duty_u16(color_list[1] * 256)
     
-    # print("blue -"+str(color_list[2]))    
+    if debug == True: print("\tblue - "+str(color_list[2]))
     blue.duty_u16(color_list[2]* 256)
 
     
 def rainbow(delay):
-    global button_pressed
     while button_pressed == False:
         for index in range(255 * 3):
             if button_pressed == True:
@@ -80,12 +80,13 @@ def rainbow(delay):
             sleep_ms(delay)
 
 def reset_led():
-    print("resetting color")
+    if debug == True: print("resetting color")
     set_color([0, 0, 0])
 
 def button_press_handler(pinfo):
-    global button_pressed
     global active_color
+    global button_pressed
+    if debug == True : print("\t\t\tbutton pressed")
     button_pressed = True
     active_color += 1
     if active_color == len(colors):
@@ -94,6 +95,7 @@ def button_press_handler(pinfo):
 # Setting up an interrupt request for the button pressanation
 button.irq(trigger=Pin.IRQ_RISING, handler=button_press_handler)
 
+# This method is used to figure out if this is a static color or a cycling wave
 def show_the_right_color(color_list):
     if color_list == ['x', 'x', 'x']:
         rainbow(10)
@@ -102,18 +104,12 @@ def show_the_right_color(color_list):
     else:
         set_color(color_list)
 
-
+# we create a list of the colors, so we can call the value from the dict, plus also print name if we need to
 color_list = list(colors.keys())
-print(color_list)
+
 while True:
-    #  set_color(list(colors.values())[active_color])
     if button_pressed == True:
         button_pressed = False
         reset_led()
-        print("showing "+color_list[active_color])
-        show_the_right_color((colors[color_list[active_color]]))
-        print("button was pressed")
-  
-# def main():
-
-# main()
+        if debug == True: print("showing "+color_list[active_color])
+        show_the_right_color((colors[color_list[active_color]]))  
